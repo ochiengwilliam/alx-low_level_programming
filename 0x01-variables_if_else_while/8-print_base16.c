efe13f54521e5a690f2b557c77b03376549d370c
ochiengwilliam <ochiengwilliam79@gmail.com>
2022-03-11 15:32:30 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+*main - beginning of program
+*
+*Description: prints number base 16 followed by a blank line
+*
+*Return: 0
+*/
+int main(void)
+{
+char n;
+char a;
+for (n = 0; n <= 9; n++)
+{
+putchar(n);
+}
+for (a = a; a <= f; a++)
+{
+putchar(a);
+}
+putchar(n);
+return (0);
+}
+
