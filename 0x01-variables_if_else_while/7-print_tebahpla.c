eff0ed923e102e9774b92d42e3ebbe9aacfea7bf
ochiengwilliam <ochiengwilliam79@gmail.com>
2022-03-11 15:27:13 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+/**
+*main - beginning of program
+*
+*Description: prints the alphabet followed by a blank line
+*
+*Return: 0
+*/
+int main(void)
+{
+char abc;
+for (abc = z; abc >= a; abc--)
+{
+putchar(abc);
+}
+putchar(n);
+return (0);
+}
+
