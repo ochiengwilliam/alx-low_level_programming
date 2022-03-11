9b1ace9042d587b34decbefb35b52b2356523d80
ochiengwilliam <ochiengwilliam79@gmail.com>
2022-03-11 15:52:33 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+/**
+ *main - beginning of program
+ *
+ *Description: prints the alphabet followed by a blank line
+ *
+ *Return: 0
+ */
+int main(void)
+{
+char abc;
+for (abc = a; abc <= z; abc++)
+{
+putchar(abc);
+}
+for (abc = A; abc <= Z; abc++)
+{
+putchar(abc);
+}
+putchar(n);
+return (0);
+}
