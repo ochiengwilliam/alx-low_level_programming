418f8aa0496a4b24c993fbb143238a7fbff64f79
ochiengwilliam <ochiengwilliam79@gmail.com>
2022-03-15 13:15:24 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
+* print_alphabet - entry point
+*Description: show the alphabet
+* Return
 */
-int _putchar(char c)
+void print_alphabet(void)
 {
-  return (write(1, &c, 1));
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
 }
