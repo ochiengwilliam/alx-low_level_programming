b0f31fb1c0d37c38ddbf4643f91cb26657a345ee
ochiengwilliam <ochiengwilliam79@gmail.com>
2022-03-15 11:47:43 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,24 @@
 #include "main.h"
 
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
+* main - entry point
 *
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
+* Description: Prints Holberton with _putchar
+*
+* Return: always 0
 */
-int _putchar(char c)
+
+int main(void)
 {
-  return (write(1, &c, 1));
+int i = 0;
+char c;
+char s[] = "main.h\n";
+
+while (i <= 9)
+{
+c = s[i];
+_putchar(c);
+i++;
+}
+return (0);
 }
