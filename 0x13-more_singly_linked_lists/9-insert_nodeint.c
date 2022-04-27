23ecf12c0dba4929f9d989ffeebe8252518b967f
ochiengwilliam <ochiengwilliam79@gmail.com>
2022-04-27 16:19:19 -0700

    67

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 listint_t *new_node, *temp;
 unsigned int i = 0;
-
 if (*head == NULL && idx != 0)
 return (NULL);
 if (idx != 0)
