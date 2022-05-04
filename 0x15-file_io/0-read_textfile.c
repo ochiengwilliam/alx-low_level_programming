18939f21b86baa64549db67177d41dc6fba23146
ochiengwilliam <ochiengwilliam79@gmail.com>
2022-05-04 01:11:58 -0700

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,42 +1,37 @@
 #include "main.h"
 /**
-* read_textfile - reads a text file
-* @filename: path of the file to read
+* read_textfile - reads a text file and prints it to the standard output
+* @filename: name of the file to be read
 * @letters: number of letters to read and print
-* Return: number of letters readed.
+* Return: the number of letters printed, or 0 if it failed
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *buffer;
-int count, fd_o, fd_r;
-if (filename == NULL)
+int fd;
+int i, y;
+char *buf;
+if (!filename)
 return (0);
-/* Open file and get file descriptor */
-fd_o = open(filename, O_RDONLY);
-if (fd_o == -1)
+fd = open(filename, O_RDONLY);
+if (fd < 0)
 return (0);
-/* allocate memory in buffer to read contents of fd */
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
+buf = malloc(sizeof(char) * letters);
+if (!buf)
 return (0);
-/* read contents of fd */
-fd_r = read(fd_o, buffer, letters);
-if (fd_r == 0)
+i = read(fd, buf, letters);
+if (i < 0)
 {
-free(buffer);
-close(fd_o);
+free(buf);
 return (0);
 }
-/* write to standard output */
-count = write(STDOUT_FILENO, buffer, fd_r);
-if (count == -1)
+buf[i] = '\0';
+close(fd);
+y = write(STDOUT_FILENO, buf, i);
+if (y < 0)
 {
-free(buffer);
-close(fd_r);
-close(fd_o);
+free(buf);
 return (0);
 }
-close(fd_r);
-close(fd_o);
-return (count);
+free(buf);
+return (y);
 }
